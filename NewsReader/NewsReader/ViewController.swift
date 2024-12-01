3229bc962bfaf3aa8775425649071be61be7c25f
Sergey Bulyno <s.bulyno@redrockapps.com>
2024-12-01 22:24:50 +0300

    Create empty project without story board.

---
+++ NewsReader/NewsReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsReader
+//
+//  Created by Sergey Bulyno on 12/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
