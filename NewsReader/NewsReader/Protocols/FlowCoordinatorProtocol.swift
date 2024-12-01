1eba866b133c125d4c0c2026093ad8f3d88797db
Sergey Bulyno <s.bulyno@redrockapps.com>
2024-12-01 22:39:43 +0300

    Add flow coordinator and view controller protocols.

---
+++ NewsReader/NewsReader/Protocols/FlowCoordinatorProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  FlowCoordinatorProtocol.swift
+//  NewsReader
+//
+//  Created by Sergey Bulyno on 12/1/24.
+//
+
+import UIKit
+
+protocol FlowCoordinatorProtocol {
+    var navigationController: UINavigationController { get set}
+    func start()
+    func stop()
+    //associatedtype T
+}
---
+++ NewsReader/NewsReader/Protocols/ViewControllerProtocol.swift
@@ -0,0 +1,10 @@
+//
+//  ViewControllerProtocol.swift
+//  NewsReader
+//
+//  Created by Sergey Bulyno on 12/1/24.
+//
+
+protocol ViewControllerProtocol {
+    
+}
