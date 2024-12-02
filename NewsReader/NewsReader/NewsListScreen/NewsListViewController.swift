bc360c35178cfa7c9bec15c94cc1b1f4b0618dca
Sergey Bulyno <s.bulyno@redrockapps.com>
2024-12-02 12:32:33 +0300

    Add loading indicator. Fix parsing error.

--- NewsReader/NewsReader/NewsListScreen/NewsListViewController.swift
+++ NewsReader/NewsReader/NewsListScreen/NewsListViewController.swift
@@ -13,6 +13,9 @@ class NewsListViewController: UIViewController {
     init(_ newsViewModel: NewsListViewModel) {
         viewModel = newsViewModel
         super.init(nibName: nil, bundle: nil)
+        viewModel.isLoadingClosure = { [weak self] isLoading in
+            self?.showLoading(isLoading)
+        }
     }
     
     required init?(coder: NSCoder) {
@@ -21,10 +24,30 @@ class NewsListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        viewModel.fetchData()
+        view.backgroundColor = .white
         // Do any additional setup after loading the view.
     }
     
-
+//MARK: Loading indicator
+    private var loadingIndicator: UIActivityIndicatorView?
+    
+    private func showLoading(_ isLoading: Bool) {
+        if isLoading {
+            loadingIndicator = UIActivityIndicatorView(style: .medium)
+            loadingIndicator?.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
+            loadingIndicator?.center = view.center
+            loadingIndicator?.color = .gray
+            if let loadingIndicator {
+                view.addSubview(loadingIndicator)
+                view.bringSubviewToFront(loadingIndicator)
+            }
+            loadingIndicator?.startAnimating()
+        } else {
+            loadingIndicator?.stopAnimating()
+            loadingIndicator?.removeFromSuperview()
+            loadingIndicator = nil
+        }
+    }
 }
 
--- NewsReader/NewsReader/NewsListScreen/NewsListViewModel.swift
+++ NewsReader/NewsReader/NewsListScreen/NewsListViewModel.swift
@@ -7,53 +7,70 @@
 //
 
 import Foundation
+typealias VoidClosure = () -> Void
+typealias ResultClosure = (Bool) -> Void
+//typealias NewsItemsClosure = (Bool) -> Void
+
 
 protocol ViewModelProtocol {
     func fetchData()
+    
+    var isLoadingClosure: ResultClosure? { get set }
+    //var reloadData: (([NewsItem])->Void)? { get set }
 }
 
 class NewsListViewModel {
+    var isLoadingClosure: ((Bool) -> Void)?
+    //TODO: use combine and observable
+    var isLoading: Bool = false {
+        didSet {
+            self.isLoadingClosure?(isLoading)
+        }
+    }
+    
     private let services: NewsListServices
     
     init(newsServices: NewsListServices) {
         services = newsServices
-        fetchData()
     }
-    
-    
 }
 
 extension NewsListViewModel: ViewModelProtocol {
     //MARK: - Fetch data
     func fetchData() {
         let sources = self.getEnabledSources()
+        isLoading = sources.count > 0
         for source in sources {
             Task {
-                   do {
-                       let items = try await services.parser.parseFeed(url: source.url,
-                                                                       sourceName: source.name)
-                       print("\(Thread.isMainThread) ")
-                       print("\(items)")
-                   } catch let error {
-                       print((error as? RSSError)?.localizedDescription ?? "")
-                   }
-               }
+                do {
+                    let items = try await RSSParser().parseFeed(url: source.url,
+                                                                    sourceName: source.name)
+//                    print("\(Thread.isMainThread) ")
+//                    print("\(items)")
+                } catch let error {
+                    
+                    print((error as? RSSError)?.localizedDescription ?? "")
+                }
+                DispatchQueue.main.async {[weak self] in
+                    self?.isLoading = false
+                }
+            }
         }
     }
-
+    
     func getEnabledSources() -> [NewsSource] {
         let allSources = [
-//                NewsSource(name: "Ведомости",
-//                                          url: "https://www.vedomosti.ru/info/rss",
-//                                          isEnabled: true),
+//                            NewsSource(name: "Ведомости",
+//                                                      url: "https://www.vedomosti.ru/info/rss",
+//                                                      isEnabled: true),
             NewsSource(name: "Лента.ру",
                        url: "http://lenta.ru/rss",
                        isEnabled: true),
-                          NewsSource(name: "RBC",
-                                     url:"http://static.feed.rbc.ru/rbc/internal/rss.rbc.ru/rbc.ru/news.rss",
-                                     isEnabled: true)
+            NewsSource(name: "RBC",
+                       url:"http://static.feed.rbc.ru/rbc/internal/rss.rbc.ru/rbc.ru/news.rss",
+                       isEnabled: true)
         ]
-
+        
         return allSources
     }
 }
