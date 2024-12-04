0d0f40c00c5e343a96e55192367dc6efac51038f
Sergey Bulyno <s.bulyno@redrockapps.com>
2024-12-04 19:21:48 +0300

    Change navbar style.

--- NewsReader/NewsReader/AppDelegate.swift
+++ NewsReader/NewsReader/AppDelegate.swift
@@ -13,7 +13,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        changeAppearance()
         return true
+    }
+    
+    private func changeAppearance() {
+        UINavigationBar.appearance().barTintColor = .darkGray
+        UINavigationBar.appearance().tintColor = .darkGray
+        UIBarButtonItem.appearance().tintColor = .gray
     }
 
     // MARK: UISceneSession Lifecycle
